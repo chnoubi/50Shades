c9fe8a89eb79573465ce0df5683e4801358bd33b
ronaldhollander <ronald.hollander@student.ehb.be>
2016-11-25 17:09:09 +0100

    CheckGrades icons
    Added CheckGrades class to handle button grades
    Refactored grade buttons in QuestionOneVC to work with CheckGrades class

---
+++ 50ShadesOfBurgers/Model/CheckGrades.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _50ShadesOfBurgers
+{
+	public class CheckGrades
+	{
+
+		private bool[] selectedGrades = new bool[4];
+
+		public bool[] SelectedGrades
+		{
+			get
+			{
+				return selectedGrades;
+			}
+			set
+			{
+				selectedGrades = value;
+			}
+
+		}
+
+		public CheckGrades()
+		{
+			
+		}
+
+		public void grade(nint tag)
+		{
+			switch (tag)
+			{
+				case 1:
+					{
+						if (!this.SelectedGrades[0])
+						{
+							
+							this.SelectedGrades[0] = true;
+						}
+						else {
+							this.SelectedGrades[0] = false;
+							this.SelectedGrades[1] = false;
+							this.SelectedGrades[2] = false;
+							this.SelectedGrades[3] = false;
+						}
+						break;
+					}
+			/*	case 2:
+					{
+						if (!selectedGrade2)
+						{
+							btnGrade2.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade1.SetImage(selectedGradeImg, UIControlState.Normal);
+							selectedGrade1 = true;
+							selectedGrade2 = true;
+						}
+						else {
+							btnGrade2.SetImage(unselectedGradeImg, UIControlState.Normal);
+							btnGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
+							btnGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+							selectedGrade2 = false;
+							selectedGrade3 = false;
+							selectedGrade4 = false;
+						}
+						break;
+					}
+				case 3:
+					{
+						if (!selectedGrade3)
+						{
+							btnGrade3.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade2.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade1.SetImage(selectedGradeImg, UIControlState.Normal);
+							selectedGrade1 = true;
+							selectedGrade2 = true;
+							selectedGrade3 = true;
+						}
+						else {
+							btnGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
+							btnGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+							selectedGrade3 = false;
+							selectedGrade4 = false;
+						}
+						break;
+					}
+				case 4:
+					{
+						if (!selectedGrade4)
+						{
+							btnGrade4.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade3.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade2.SetImage(selectedGradeImg, UIControlState.Normal);
+							btnGrade1.SetImage(selectedGradeImg, UIControlState.Normal);
+							selectedGrade1 = true;
+							selectedGrade2 = true;
+							selectedGrade3 = true;
+							selectedGrade4 = true;
+						}
+						else {
+							btnGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+							selectedGrade4 = false;
+						}
+						break;
+					}*/
+
+			}
+
+		}
+
+
+	}
+}
--- 50ShadesOfBurgers/QuestionOneViewController.cs
+++ 50ShadesOfBurgers/QuestionOneViewController.cs
@@ -10,8 +10,13 @@ namespace _50ShadesOfBurgers
 {
 	partial class QuestionOneViewController : UIViewController
 	{
-        AppDelegate ad = (AppDelegate)UIApplication.SharedApplication.Delegate;
+
+
+		AppDelegate ad = (AppDelegate)UIApplication.SharedApplication.Delegate;
 		UIImage selectedGradeImg, unselectedGradeImg;
+
+		//create an instance for the bun grades
+		CheckGrades bunGrades, meatGrades, sauceGrades, saladGrades;
 
         public List<String> question1, question2;
 		public QuestionOneViewController (IntPtr handle) : base (handle)
@@ -26,7 +31,21 @@ namespace _50ShadesOfBurgers
             question1 = new List<String>();
             question2 = new List<String>();
 
-            this.setupPickers();
+			//initialize de grades (default false)
+			bunGrades = new CheckGrades();
+			meatGrades = new CheckGrades();
+			sauceGrades = new CheckGrades();
+			saladGrades = new CheckGrades();
+
+			//   this.setupPickers();
+
+			//handle bun buttons
+			btnBunGrade1.TouchUpInside += handleBunGrades;
+			btnBunGrade2.TouchUpInside += handleBunGrades;
+			btnBunGrade3.TouchUpInside += handleBunGrades;
+			btnBunGrade4.TouchUpInside += handleBunGrades;
+
+
             btnNext.TouchUpInside += BtnNext_TouchUpInside;
         }
 
@@ -35,10 +54,13 @@ namespace _50ShadesOfBurgers
 			//    ad.reponses.ReponseQuestId1 = (int) pickerPain.SelectedRowInComponent(0) + 1;
 			//    ad.reponses.ReponseQuestId2 = (int)pickerViande.SelectedRowInComponent(0) + 1;
 
-			//ad.connection.updateReponses(ad.reponses);
+			//ad.connection.updateReponses(ad.reponses);			
+
+
+
 
 			this.PerformSegue("goToQuest5", this);
-        }
+		}
 
         public override void ViewWillAppear(bool animated)
         {
@@ -50,56 +72,152 @@ namespace _50ShadesOfBurgers
 			selectedGradeImg = UIImage.FromFile("selectedGrade.png");
 			selectedGradeImg = selectedGradeImg.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
 
-			setupGradeButtons();
-
-
 
 		}
 
-		private void setupGradeButtons()
+		public override void ViewDidAppear(bool animated)
 		{
+			base.ViewDidAppear(animated);
+
+			checkGradeButtons();
+		}
+
+		//checks based on the bool array of the CheckGrades instances if the grades should be selected or not
+		private void checkGradeButtons()
+		{
+
 			//bun
-			btnBunGrade1.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnBunGrade2.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnBunGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnBunGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+			for (int i = 1; i <= bunGrades.SelectedGrades.Length;i++)
+			{
+				
+				UIButton btn = (UIButton) (this.View.ViewWithTag((nint)i));
+				if (bunGrades.SelectedGrades[i-1])
+				{
+					btn.SetImage(selectedGradeImg, UIControlState.Normal);
+				}
+				else {
+					btn.SetImage(unselectedGradeImg, UIControlState.Normal);
+				}
+			}
 
 			//meat
-			btnMeatGrade1.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnMeatGrade2.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnMeatGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnMeatGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+			for (int i = 5; i <= meatGrades.SelectedGrades.Length + 4;i++)
+			{
+				
+				UIButton btn = (UIButton) (this.View.ViewWithTag((nint)i));
+				if (meatGrades.SelectedGrades[i-5])
+				{
+					btn.SetImage(selectedGradeImg, UIControlState.Normal);
+				}
+				else {
+					btn.SetImage(unselectedGradeImg, UIControlState.Normal);
+				}
+			}
 
 			//sauce
-			btnSauceGrade1.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSauceGrade2.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSauceGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSauceGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+			for (int i = 9; i <= sauceGrades.SelectedGrades.Length + 8; i++)
+			{
+
+				UIButton btn = (UIButton)(this.View.ViewWithTag((nint)i));
+				if (sauceGrades.SelectedGrades[i - 9])
+				{
+					btn.SetImage(selectedGradeImg, UIControlState.Normal);
+				}
+				else {
+					btn.SetImage(unselectedGradeImg, UIControlState.Normal);
+				}
+			}
 
 			//salad
-			btnSaladGrade1.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSaladGrade2.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSaladGrade3.SetImage(unselectedGradeImg, UIControlState.Normal);
-			btnSaladGrade4.SetImage(unselectedGradeImg, UIControlState.Normal);
+			for (int i = 13; i <= saladGrades.SelectedGrades.Length + 12; i++)
+			{
+
+				UIButton btn = (UIButton)(this.View.ViewWithTag((nint)i));
+				if (saladGrades.SelectedGrades[i - 13])
+				{
+					btn.SetImage(selectedGradeImg, UIControlState.Normal);
+				}
+				else {
+					btn.SetImage(unselectedGradeImg, UIControlState.Normal);
+				}
+			}
 		}
 
 
-        public void setupPickers()
-        {
-            question1.Add(ad.questions[0].QuestRep1);
-            question1.Add(ad.questions[0].QuestRep2);
-            question1.Add(ad.questions[0].QuestRep3);
-            question1.Add(ad.questions[0].QuestRep4);
+		void handleBunGrades(object sender, EventArgs e)
+		{
+			UIButton btn = (UIButton)sender;
+			var tag = btn.Tag;
 
-            question2.Add(ad.questions[1].QuestRep1);
-            question2.Add(ad.questions[1].QuestRep2);
-            question2.Add(ad.questions[1].QuestRep3);
-            question2.Add(ad.questions[1].QuestRep4);
+			bunGrades.grade(tag);
 
-       //     pickerPain.Model = new QuestionPickerViewModel<String>(question1);
-         //   pickerViande.Model = new QuestionPickerViewModel<String>(question2);
+			checkGradeButtons();
+
+		}
+
+		void handleMeatGrades(object sender, EventArgs e)
+		{
+			UIButton btn = (UIButton)sender;
+			var tag = btn.Tag;
+
+			//harmonize tags for grade method
+			tag = tag - 4;
+
+			meatGrades.grade(tag);
+
+			checkGradeButtons();
+
+		}
+
+		void handleSauceGrades(object sender, EventArgs e)
+		{
+			UIButton btn = (UIButton)sender;
+			var tag = btn.Tag;
+						
+			//harmonize tags for grade method
+			tag = tag - 8;
+
+			sauceGrades.grade(tag);
+
+			checkGradeButtons();
+
+		}
+
+		void handleSaladGrades(object sender, EventArgs e)
+		{
+			UIButton btn = (UIButton)sender;
+			var tag = btn.Tag;
+
+			//harmonize tags for grade method
+			tag = tag - 12;
+
+			saladGrades.grade(tag);
+
+			checkGradeButtons();
+
+		}
 
 
-        }
-    }
+
+
+		/*   public void setupPickers()
+		   {
+			   question1.Add(ad.questions[0].QuestRep1);
+			   question1.Add(ad.questions[0].QuestRep2);
+			   question1.Add(ad.questions[0].QuestRep3);
+			   question1.Add(ad.questions[0].QuestRep4);
+
+			   question2.Add(ad.questions[1].QuestRep1);
+			   question2.Add(ad.questions[1].QuestRep2);
+			   question2.Add(ad.questions[1].QuestRep3);
+			   question2.Add(ad.questions[1].QuestRep4);
+
+			   pickerPain.Model = new QuestionPickerViewModel<String>(question1);
+			  pickerViande.Model = new QuestionPickerViewModel<String>(question2);
+
+
+		   }*/
+
+
+	}
 }
